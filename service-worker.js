7c27859b89b93c8e226abdf8eb520cc70eee28d4
38063 <xeilen1112@gmail.com>
2020-10-25 23:06:02 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.2b0af61084fbdad2723f57bea79aa38c.js"
 );
 
 self.addEventListener('message', (event) => {
